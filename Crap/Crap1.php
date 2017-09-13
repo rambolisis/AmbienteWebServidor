42725bffab01f7c0effea28e704e482d6865cdba
rambolisis <31830857+rambolisis@users.noreply.github.com>
2017-09-12 18:06:55 -0600

    Semana 1

---
+++ Crap/Crap1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UFT-8">
+<title>Primera Clase</title>
+</head>
+<body>
+<h1>Mi Sitio</h1>
+<h2><?php echo date("Y-m-d H:i:s"); ?></h2>
+<p> AWS trata de crear sitios con apoyo de servidor pero sin meterle Java</p>
+
+<ul><!-- clase.php-->
+<li><a href="tiempo.html">Tirmpo Libre</a></li>
+<li><a href="vacaciones.html">Vacaciones</a></li>
+</ul>
+<hr>
+<?php echo "<b> Dise;o".date("Y")."</b>"; ?>
+</body>
+</html>
---
+++ Crap/Variables.php
@@ -0,0 +1,10 @@
+<?php /* Variables*/
+echo "<h1>Variables</h>";
+$nombre="Ricardo";
+$edad=24;
+echo "<h3> Me llamo $nombre y tengo $edad a;os </h3>";
+echo "<h2> si uso comilla simple no se van a ver las variables </h2>";
+echo '<h3> Me llamo $nombre y tengo $edad a;os </h3>';
+$edadMeses=$edad*12;
+echo "<p>Edad en meses: $edadMeses</p>";
+?>
